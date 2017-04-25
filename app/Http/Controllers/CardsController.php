2ebd320d46c4c7785f0f6c86f8212f7a5b0c61f5
Mark <marktomlin2709@gmail.com>
2017-04-25 12:05:55 +0100

    Added cards view,controller and also table to mysql

---
+++ app/Http/Controllers/CardsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class CardsController extends Controller
+{
+    public function index()
+    {
+        $cards = DB::table('cards')->get();
+        return view('cards.index', compact('cards'));    
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,3 +23,4 @@ Route::get('about', function() {
 
 Route::get('/', 'PagesController@home');
 Route::get('about', 'PagesController@about');
+Route::get('cards', 'CardsController@index');
