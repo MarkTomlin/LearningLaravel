3f19134d165174ad9b661b460c1edb79db4aaccb
Mark <marktomlin2709@gmail.com>
2017-04-26 11:40:23 +0100

    Changed userstableseeder to use the model factory which has been updated to comply with the users table. so when DB seed it creates 5 users table inserts with Faker.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,16 +12,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        /*
         DB::table('users')->insert([
            'username' => str_random(10),
            'email' => str_random(10).'@gmail.com',
            'password' => bcrypt('secret'),
         ]); 
+        */ 
 
-        /*
-        factory(App\User::class, 5)->create()->each(function ($u) {
-            $u->posts()->save(factory(App\Post::class)->make());
-        });*/
+        factory(App\User::class, 5)->create();
 
     }
 }
